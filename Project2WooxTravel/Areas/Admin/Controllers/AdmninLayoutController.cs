7d80c5fd01cd4b4754005e0a9189d4ec854f3b4f
Büşra ÖLMEZ <olmezbusra396@gmail.com>
2024-10-15 16:06:33 +0300

    ....

---
+++ Project2WooxTravel/Areas/Admin/Controllers/AdmninLayoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2WooxTravel.Areas.Admin.Controllers
+{
+    public class AdmninLayoutController : Controller
+    {
+        // GET: Admin/AdmninLayout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
