b19c193d616a24b3c94cd017e88ab1e5fb7b728c
Büşra ÖLMEZ <olmezbusra396@gmail.com>
2024-10-15 15:11:28 +0300

    Add project files.

---
+++ Project2WooxTravel/Entities/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2WooxTravel.Entities
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string Phone{ get; set; }
+        public string Email{ get; set; }
+        public string Addresss{ get; set; }
+        public string MapLocation{ get; set; }
+    }
+}
