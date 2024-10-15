11d2ea562e61d22efc8ec7db24122f001cc64bdb
Büşra ÖLMEZ <olmezbusra396@gmail.com>
2024-10-15 17:34:07 +0300

    ...

--- Project2WooxTravel/Areas/Admin/Controllers/AdmninLayoutController.cs
+++ Project2WooxTravel/Areas/Admin/Controllers/AdmninLayoutController.cs
@@ -13,5 +13,25 @@ namespace Project2WooxTravel.Areas.Admin.Controllers
         {
             return View();
         }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
